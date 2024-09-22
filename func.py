420c3ed9305fc06388b639246655e4b6bb0f321a
Sudhar <rajusudhar1616@gmail.com>
2024-09-22 18:04:59 +0530

    update functions with match_fingerprint()

--- func.py
+++ func.py
@@ -1,42 +1,36 @@
-import hashlib
+import os
 import cv2
-import numpy as np
+
+def match_fingerprint(key_fingerprint):
+    best_score = 0
+    matched_image = None
+    matched_fileName = None
+    kp1, kp2, mp = None, None, None
+
+    for file in os.listdir("Real"):
+        fingerprint_image = cv2.imread("Real/" + file)
+        sift = cv2.SIFT.create()
+
+        keypoints_1, descriptors_1 = sift.detectAndCompute(key_fingerprint, None)
+        keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
+
+        matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
+                                        {}).knnMatch(descriptors_1, descriptors_2, k=2)
+
+        match_point = []
+        for p, q in matches:
+            if p.distance < 0.1 * q.distance:
+                match_point.append(p)
+
+        keypoints = min(len(keypoints_2),len(keypoints_1))
 
 
-def generate_minutiae_fingerprint_hash(img_file) -> str:
+        if len(match_point) / keypoints * 100 > best_score:
+            best_score = len(match_point) / keypoints * 100
+            matched_fileName = file
+            matched_image = fingerprint_image
+            kp1, kp2, mp = keypoints_1, keypoints_2, match_point
 
 
-    sift = cv2.SIFT.create()
-    keypoints, descriptors = sift.detectAndCompute(img_file, None)
-
-    if not all(isinstance(kp, cv2.KeyPoint) for kp in keypoints):
-        raise ValueError("keypoints must be a list of cv2.KeyPoint objects")
-    if not isinstance(descriptors, np.ndarray):
-        raise ValueError("descriptors must be a numpy array")
-    
-    def hash_data(data) -> str:
-        combined_string = data.tobytes()
-        hash_object = hashlib.sha256(combined_string)
-        return hash_object.hexdigest()
-
-    keypoints_data = [(kp.pt[0], kp.pt[1], kp.angle) for kp in keypoints]
-    descriptors_data = descriptors.flatten()
-    combined_data = np.concatenate([np.array(keypoints_data).flatten(), descriptors_data])
-    fingerprint_hash = hash_data(combined_data)
-
-    return fingerprint_hash
-
-
-File1 = cv2.imread("Real/1__M_Left_ring_finger.BMP")
-testfile = cv2.imread("Altered/Altered-Easy/1__M_Left_ring_finger_Zcut.BMP")
-
-hash_is = generate_minutiae_fingerprint_hash(File1)
-newhash = generate_minutiae_fingerprint_hash(testfile)
-
-
-print(hash_is)
-print(newhash)
-
-cv2.imshow("og", File1)
-cv2.imshow("Test",testfile)
-cv2.waitKey(0)
+    print("BEST MATCH : ", matched_fileName)
+    print("BEST SCORE : ", str(best_score))
